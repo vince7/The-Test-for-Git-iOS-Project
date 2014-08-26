23b5ae2d1aa959b3b218a78841fdec91b0da3361
vins7 <Vincerachaka@Onneiles-MacBook-Pro-3.local>
2014-08-27 00:57:37 +0100

    Initial Commit

---
+++ The Git TestTests/The_Git_TestTests.m
@@ -0,0 +1,34 @@
+//
+//  The_Git_TestTests.m
+//  The Git TestTests
+//
+//  Created by Onneile Rachaka on 27/08/2014.
+//  Copyright (c) 2014 Onneile Rachaka. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface The_Git_TestTests : XCTestCase
+
+@end
+
+@implementation The_Git_TestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
