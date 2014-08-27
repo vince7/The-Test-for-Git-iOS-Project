3d65a006477ff50afef5270495f6b2d7524f1ed7
vins7 <Vincerachaka@Onneiles-MacBook-Pro-3.local>
2014-08-27 01:08:25 +0100

    added a test class

---
+++ The Git Test/ORTest.h
@@ -0,0 +1,13 @@
+//
+//  ORTest.h
+//  The Git Test
+//
+//  Created by Onneile Rachaka on 27/08/2014.
+//  Copyright (c) 2014 Onneile Rachaka. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ORTest : NSObject
+
+@end
---
+++ The Git Test/ORTest.m
@@ -0,0 +1,13 @@
+//
+//  ORTest.m
+//  The Git Test
+//
+//  Created by Onneile Rachaka on 27/08/2014.
+//  Copyright (c) 2014 Onneile Rachaka. All rights reserved.
+//
+
+#import "ORTest.h"
+
+@implementation ORTest
+
+@end
